782a1d51bdbe35f5258e2c874d67e42b028d4cdf
conradoramalho <conrado.ramalho94@gmail.com>
2018-06-01 16:06:23 -0300

    feat: add user login mutation

---
+++ src/graphql/mutations/user/index.js
@@ -0,0 +1,11 @@
+import addUser from './add';
+import removeUser from './remove';
+import updateUser from './update';
+import loginUser from './login';
+
+export default {
+  addUser,
+  removeUser,
+  updateUser,
+  loginUser,
+};
---
+++ src/graphql/mutations/user/login.js
@@ -0,0 +1,40 @@
+import { GraphQLString, GraphQLNonNull } from 'graphql';
+import { userLoginType } from '../../types/user';
+import UserModel from '../../../models/user';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { secretKey } from '../../../configs/constants';
+
+const login = {
+  type: GraphQLString,
+  args: {
+    data: {
+      name: 'data',
+      type: new GraphQLNonNull(userLoginType),
+    },
+  },
+  async resolve(root, { data }) {
+    const { email, password } = data;
+
+    const user = await UserModel.findOne({ email }, {}, err => {
+      if (err) throw new Error('Not user with this e-mail was fond!');
+    });
+
+    const isCorrectPassword = bcrypt.compareSync(password, user.password);
+
+    if (!isCorrectPassword) throw new Error('Incorrect password!');
+
+    const token = generateToken(user);
+
+    return token;
+  },
+};
+
+const generateToken = async ({ id, name, lastName, type, email }) => {
+  const user = { id, name, lastName, type, email };
+  const token = await jwt.sign({ user }, secretKey, { expiresIn: '1h' });
+
+  return token;
+};
+
+export default login;
